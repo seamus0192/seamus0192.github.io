0fb7a3cde7bdb0d792868adc938a614479c7848c
Seamus Q O'Malley <63891206+seamus0192@users.noreply.github.com>
2023-03-09 09:00:59 -0800

    contact/ css adj

--- world.js
+++ world.js
@@ -1,8 +1,11 @@
 window.onload = function (){
-    var visitedCountries = ["Greece"]
+    // create a list of country id's I have visited
+    var visitedCountries = ["JP"]
     var c = document.getElementById("worldMap").contentDocument;
     var ids = c.querySelectorAll('[id]');
     console.log(ids)
-    var cntry = c.getElementById("JP");
+    // iterate throughlist, filling
+    //some countries have multiple elements, such as JP below. Need to solve that.
+    var cntry = c.getElementById(visitedCountries);
     cntry.setAttribute("style", "fill: green;");
 }
