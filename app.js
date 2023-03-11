bd27b24ac6d7c9f634d366eea57f98b0bec654aa
Seamus Q O'Malley <63891206+seamus0192@users.noreply.github.com>
2023-03-11 12:46:26 -0800

    js for map

--- app.js
+++
@@ -1,3 +0,0 @@
-
-gsap.fromTo('#top-pg',{opacity: 0},{opacity: 1, duration: 5});
-gsap.fromTo('img',{y: '-10%'},{y: '0%', duration: 2})
--- world.js
+++ world.js
@@ -1,11 +1,28 @@
 window.onload = function (){
-    // create a list of country id's I have visited
-    var visitedCountries = ["JP"]
+    // create a list of countries I have visited
+    var visitedCountries = ["Mexico", "United States", "Canada", "Kenya", "Tanzania", "Croatia", 
+                                "Greece", "Malaysia", "Japan", "Costa Rica", "Puerto Rico"]
+    colorMap(visitedCountries, "slateblue")
+
+    //list of my top 5 countries I want to visit next
+    var futureTravel = ["Ireland", "Brazil", "South Africa", "Dominican Republic", "South Korea"]
+    colorMap(futureTravel, "coral")
+    
+}
+
+function colorMap(places, color) {
     var c = document.getElementById("worldMap").contentDocument;
-    var ids = c.querySelectorAll('[id]');
-    console.log(ids)
     // iterate throughlist, filling
     //some countries have multiple elements, such as JP below. Need to solve that.
-    var cntry = c.getElementById(visitedCountries);
-    cntry.setAttribute("style", "fill: green;");
+    var countries = []
+    places.forEach(place => countries.push(c.getElementsByClassName(place)));
+
+    console.log(countries)
+    for (let i = 0; i < countries.length; i++){
+        // need seocond loop because some countries have multiple parts
+        for (country of countries[i]) { 
+            ///console.log(country)
+            country.setAttribute("style", "fill: " + color + ";"); 
+        }
+    }
 }
